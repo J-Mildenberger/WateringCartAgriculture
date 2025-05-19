593a7d76f58287f8a2178a320a889fc6dbc7efc1
Julian <95878604+J-Mildenberger@users.noreply.github.com>
2025-05-19 23:07:57 +0200

    enabled NVIC preempt priorities

--- playground/Core/Src/stm32f4xx_hal_msp.c
+++ playground/Core/Src/stm32f4xx_hal_msp.c
@@ -70,9 +70,13 @@ void HAL_MspInit(void)
   __HAL_RCC_SYSCFG_CLK_ENABLE();
   __HAL_RCC_PWR_CLK_ENABLE();
 
-  HAL_NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_0);
+  HAL_NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_3);
 
   /* System interrupt init*/
+  /* SVCall_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(SVCall_IRQn, 0, 1);
+  /* DebugMonitor_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DebugMonitor_IRQn, 0, 1);
 
   /* USER CODE BEGIN MspInit 1 */
 
